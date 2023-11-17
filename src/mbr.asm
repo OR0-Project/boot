8c19d8f998c43fb3dcf72897078bb86838415a26
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-11-17 14:59:13 +0100

    name changes

--- src/mbr.asm
+++ src/mbr.asm
@@ -22,6 +22,8 @@
 %define DAP_ADDRESS 0x8000
 %define DAP_SEGMENT 0x0000
 
+%define DAP_SIGNATURE 0x424F4F54
+
 
 BITS 16
 org 0x7C00
@@ -105,28 +107,26 @@ A20:    ; enable the A20 gate
 	jnz .check                      ; retry if error occurred
 	mov al, 0xDF                    ; TODO: note
 	out 0x60, al                    ; TODO: note
-
 	mov si, msg_start
 	call print_string_16
-
 	mov cx, 0x4000 - 1              ; start looking from 0x4000 to 0x4FFF
 VBE_search:
 	inc cx
 	cmp cx, 0x5000
 	je halt
-	mov edi, VBEModeInfoBlock       ; VBE data will be stored at this address
+	mov edi, VBE_mode_info_block       ; VBE data will be stored at this address
 	mov ax, 0x4F01                  ; get SuperVGA mode information - http://www.ctyme.com/intr/rb-0274.htm
 	mov bx, cx                      ; mode is saved to BX for the set command later
 	int 0x10
 	cmp ax, 0x004F                  ; return value in AX should equal 0x004F if command is supported and successful
 	jne VBE_search                  ; try next mode
-	cmp byte [VBEModeInfoBlock.BitsPerPixel], VBE_BIT_DEPTH
+	cmp byte [VBE_mode_info_block.pixel_depth], VBE_BIT_DEPTH
 	jne VBE_search                  ; try next mode if bit depth is not desired
-	cmp word [VBEModeInfoBlock.XResolution], VBE_X_RES
+	cmp word [VBE_mode_info_block.x_resolution], VBE_X_RES
 	jne VBE_search                  ; try next mode if bit x-res is not desired
-	cmp word [VBEModeInfoBlock.YResolution], VBE_Y_RES
+	cmp word [VBE_mode_info_block.y_resolution], VBE_Y_RES
 	jne VBE_search                  ; try next mode if bit y-res is not desired
-	; TODO
+
 	or bx, 0x4000                   ; use linear/flat frame buffer model (set bit 14) TODO: notes
 	mov ax, 0x4F02                  ; set SuperVGA video mode - http://www.ctyme.com/intr/rb-0275.htm
 	int 0x10
@@ -142,11 +142,10 @@ verify_32_bit_stage:
 	;start: <- loaded @DAP_ADDRESS
 	;   jmp start32                 ; jmp 32-bit-address    -> 5 bytes
 	;   nop                         ; nop                   -> 1 byte
-	;   db 0x36, 0x34               ; @(DAP_ADDRESS + 6)
-	mov ax, [DAP_ADDRESS + 6]       ; load os marker
-	cmp ax, 0x3436                  ; match magic number
+	;   dd 0x........               ; @(DAP_ADDRESS + 6)
+	mov eax, dword[DAP_ADDRESS + 6] ; load os marker
+	cmp eax, DAP_SIGNATURE          ; match magic number
 	jne sig_fail                    ; print error if magic number is not found
-
 	mov si, msg_ok                  ; print ok message on load success
 	call print_string_16
 jump_to_32_bit_stage:
@@ -156,6 +155,7 @@ jump_to_32_bit_stage:
 	or al, 0x01                     ; set protected mode bit
 	mov cr0, eax                    ; set CR0 (control register 0)
 	jmp 0x0008:DAP_ADDRESS          ; jump to 32-bit protected mode
+
 
 read_fail:
 	mov si, msg_read_fail           ; load read failure message
@@ -189,7 +189,7 @@ serial_print_16:                    ; output string in SI to screen
 
 
 align 16
-GDTR32:                             ; GDTR(Global Descriptors Table Register)
+GDTR32:                             ; GDTR (Global Descriptors Table Register)
 dw gdt32_end - gdt32 - 1            ; limit of GDT (size - 1)
 dq gdt32                            ; linear address of GDT
 
@@ -213,7 +213,7 @@ db 0x80, 0x00, 0x01, 0x00, 0xEB, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF,
 DriveNumber db 0x00
 
 
-times 476-$+$$ db 0x00              ; DAP offset
+times 476-$+$$ db 0x00              ; DAP (disk address packet) offset
 
 align 4
 DAP:
@@ -228,40 +228,40 @@ DAP:
 times 510-$+$$ db 0x00              ; end of sector offset
 dw 0xAA55                           ; magic number for bootable sector
 
-VBEModeInfoBlock: equ 0x5C00        ; VESA
+VBE_mode_info_block: equ 0x5C00        ; VESA
 ; mandatory information for all VBE revisions
-VBEModeInfoBlock.ModeAttributes         equ VBEModeInfoBlock + 0    ; DW - mode attributes
-VBEModeInfoBlock.WinAAttributes         equ VBEModeInfoBlock + 2    ; DB - window A attributes
-VBEModeInfoBlock.WinBAttributes         equ VBEModeInfoBlock + 3    ; DB - window B attributes
-VBEModeInfoBlock.WinGranularity         equ VBEModeInfoBlock + 4    ; DW - window granularity in KB
-VBEModeInfoBlock.WinSize                equ VBEModeInfoBlock + 6    ; DW - window size in KB
-VBEModeInfoBlock.WinASegment            equ VBEModeInfoBlock + 8    ; DW - window A start segment
-VBEModeInfoBlock.WinBSegment            equ VBEModeInfoBlock + 10   ; DW - window B start segment
-VBEModeInfoBlock.WinFuncPtr             equ VBEModeInfoBlock + 12   ; DD - real mode pointer to window function
-VBEModeInfoBlock.BytesPerScanLine       equ VBEModeInfoBlock + 16   ; DW - bytes per scan line
+VBE_mode_info_block.mode_attributes         equ VBE_mode_info_block + 0    ; DW - mode attributes
+VBE_mode_info_block.window_a_attributes     equ VBE_mode_info_block + 2    ; DB - window A attributes
+VBE_mode_info_block.window_b_attributes     equ VBE_mode_info_block + 3    ; DB - window B attributes
+VBE_mode_info_block.window_granularity      equ VBE_mode_info_block + 4    ; DW - window granularity in KB
+VBE_mode_info_block.window_size             equ VBE_mode_info_block + 6    ; DW - window size in KB
+VBE_mode_info_block.window_a_segment        equ VBE_mode_info_block + 8    ; DW - window A start segment
+VBE_mode_info_block.window_b_segment        equ VBE_mode_info_block + 10   ; DW - window B start segment
+VBE_mode_info_block.window_function         equ VBE_mode_info_block + 12   ; DD - real mode pointer to window function
+VBE_mode_info_block.bytes_per_scan_line     equ VBE_mode_info_block + 16   ; DW - bytes per scan line
 ; mandatory information for VBE 1.2 and above
-VBEModeInfoBlock.XResolution            equ VBEModeInfoBlock + 18   ; DW - horizontal resolution in pixels or characters
-VBEModeInfoBlock.YResolution            equ VBEModeInfoBlock + 20   ; DW - vertical resolution in pixels or characters
-VBEModeInfoBlock.XCharSize              equ VBEModeInfoBlock + 22   ; DB - character cell width in pixels
-VBEModeInfoBlock.YCharSize              equ VBEModeInfoBlock + 23   ; DB - character cell height in pixels
-VBEModeInfoBlock.NumberOfPlanes         equ VBEModeInfoBlock + 24   ; DB - number of memory planes
-VBEModeInfoBlock.BitsPerPixel           equ VBEModeInfoBlock + 25   ; DB - bits per pixel
-VBEModeInfoBlock.NumberOfBanks          equ VBEModeInfoBlock + 26   ; DB - number of banks
-VBEModeInfoBlock.MemoryModel            equ VBEModeInfoBlock + 27   ; DB - memory model type
-VBEModeInfoBlock.BankSize               equ VBEModeInfoBlock + 28   ; DB - bank size in KB
-VBEModeInfoBlock.NumberOfImagePages     equ VBEModeInfoBlock + 29   ; DB - number of image pages
-VBEModeInfoBlock.Reserved               equ VBEModeInfoBlock + 30   ; DB - reserved (0x00 for VBE 1.0-2.0, 0x01 for VBE 3.0)
+VBE_mode_info_block.x_resolution            equ VBE_mode_info_block + 18   ; DW - horizontal resolution in pixels or characters
+VBE_mode_info_block.y_resolution            equ VBE_mode_info_block + 20   ; DW - vertical resolution in pixels or characters
+VBE_mode_info_block.x_char_size             equ VBE_mode_info_block + 22   ; DB - character cell width in pixels
+VBE_mode_info_block.y_char_size             equ VBE_mode_info_block + 23   ; DB - character cell height in pixels
+VBE_mode_info_block.number_of_planes        equ VBE_mode_info_block + 24   ; DB - number of memory planes
+VBE_mode_info_block.pixel_depth             equ VBE_mode_info_block + 25   ; DB - bits per pixel
+VBE_mode_info_block.number_of_banks         equ VBE_mode_info_block + 26   ; DB - number of banks
+VBE_mode_info_block.memory_model            equ VBE_mode_info_block + 27   ; DB - memory model type
+VBE_mode_info_block.bank_size               equ VBE_mode_info_block + 28   ; DB - bank size in KB
+VBE_mode_info_block.number_of_image_pages   equ VBE_mode_info_block + 29   ; DB - number of image pages
+VBE_mode_info_block.reserved_0              equ VBE_mode_info_block + 30   ; DB - reserved (0x00 for VBE 1.0-2.0, 0x01 for VBE 3.0)
 ; direct color fields (required for direct/6 and YUV/7 memory models)
-VBEModeInfoBlock.RedMaskSize            equ VBEModeInfoBlock + 31   ; DB - size of direct color red mask in bits
-VBEModeInfoBlock.RedFieldPosition       equ VBEModeInfoBlock + 32   ; DB - bit position of lsb of red mask
-VBEModeInfoBlock.GreenMaskSize          equ VBEModeInfoBlock + 33   ; DB - size of direct color green mask in bits
-VBEModeInfoBlock.GreenFieldPosition     equ VBEModeInfoBlock + 34   ; DB - bit position of lsb of green mask
-VBEModeInfoBlock.BlueMaskSize           equ VBEModeInfoBlock + 35   ; DB - size of direct color blue mask in bits
-VBEModeInfoBlock.BlueFieldPosition      equ VBEModeInfoBlock + 36   ; DB - bit position of lsb of blue mask
-VBEModeInfoBlock.RsvdMaskSize           equ VBEModeInfoBlock + 37   ; DB - size of direct color reserved mask in bits
-VBEModeInfoBlock.RsvdFieldPosition      equ VBEModeInfoBlock + 38   ; DB - bit position of lsb of reserved mask
-VBEModeInfoBlock.DirectColorModeInfo    equ VBEModeInfoBlock + 39   ; DB - direct color mode attributes
+VBE_mode_info_block.red_mask_size           equ VBE_mode_info_block + 31   ; DB - size of direct color red mask in bits
+VBE_mode_info_block.red_field_position      equ VBE_mode_info_block + 32   ; DB - bit position of lsb of red mask
+VBE_mode_info_block.green_mask_size         equ VBE_mode_info_block + 33   ; DB - size of direct color green mask in bits
+VBE_mode_info_block.green_field_position    equ VBE_mode_info_block + 34   ; DB - bit position of lsb of green mask
+VBE_mode_info_block.blue_mask_size          equ VBE_mode_info_block + 35   ; DB - size of direct color blue mask in bits
+VBE_mode_info_block.blue_field_position     equ VBE_mode_info_block + 36   ; DB - bit position of lsb of blue mask
+VBE_mode_info_block.reserved_mask_size      equ VBE_mode_info_block + 37   ; DB - size of direct color reserved mask in bits
+VBE_mode_info_block.reserved_field_position equ VBE_mode_info_block + 38   ; DB - bit position of lsb of reserved mask
+VBE_mode_info_block.direct_color_mode       equ VBE_mode_info_block + 39   ; DB - direct color mode attributes
 ; mandatory information for VBE 2.0 and above
-VBEModeInfoBlock.PhysBasePtr            equ VBEModeInfoBlock + 40   ; DD - physical address for flat memory frame buffer
-VBEModeInfoBlock.Reserved1              equ VBEModeInfoBlock + 44   ; DD - Reserved - always set to 0
-VBEModeInfoBlock.Reserved2              equ VBEModeInfoBlock + 48   ; DD - Reserved - always set to 0
+VBE_mode_info_block.physical_base_pointer  equ VBE_mode_info_block + 40   ; DD - physical address for flat memory frame buffer
+VBE_mode_info_block.reserved_1             equ VBE_mode_info_block + 44   ; DD - Reserved - always set to 0
+VBE_mode_info_block.reserved_2             equ VBE_mode_info_block + 48   ; DD - Reserved - always set to 0
